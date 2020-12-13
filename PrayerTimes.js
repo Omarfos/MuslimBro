5f1128c4a79e954a9b48d2a8cd52a87e088c2fbe
Omar Amin <omaramin4@gmail.com>
2020-12-12 22:05:40 -0500

    Adding PNGs

---
+++ PrayerTimes.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import adhan from 'adhan'
+import * as Location from 'expo-location';
+
+
+const PrayerTime = (props) => {
+    return (
+        <Text style={styles.paragraph}>
+            {props.time}
+        </Text>
+    )
+}
+
+
+const PrayerTimes = () => {
+    const [location, setLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+        })();
+    }, []);
+
+    const params = adhan.CalculationMethod.MuslimWorldLeague();
+    let prayerTimes;
+    if (location) {
+        prayerTimes = new adhan.PrayerTimes(new adhan.Coordinates(location.coords.latitude, location.coords.longitude), new Date(), params);
+    }
+
+    return (
+
+        <View style={styles.container}>
+            {prayerTimes &&
+                <>
+                    <PrayerTime time={prayerTimes.fajr.toLocaleTimeString()} />
+                    <PrayerTime time={prayerTimes.dhuhr.toLocaleTimeString()} />
+                    <PrayerTime time={prayerTimes.asr.toLocaleTimeString()} />
+                    <PrayerTime time={prayerTimes.maghrib.toLocaleTimeString()} />
+                    <PrayerTime time={prayerTimes.isha.toLocaleTimeString()} />
+                </>
+            }
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 20,
+    },
+    paragraph: {
+        fontSize: 18,
+        textAlign: 'center',
+    },
+});
+
+export default PrayerTimes;
