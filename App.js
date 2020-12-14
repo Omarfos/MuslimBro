546ac0f5b347200294fd53e794c4c8a0f792cd08
Omar Amin <omaramin4@gmail.com>
2020-12-13 19:56:43 -0500

    Sliding between pages

--- App.js
+++ App.js
@@ -1,42 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, ScrollView, Image, TextInput, Button, SafeAreaView, FlatList } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import adhan from 'adhan'
 import * as Location from 'expo-location';
 import PrayerTimes from './PrayerTimes';
-import Pages from './Quran';
+import Pages from './Pages';
+import Quran from './Quran';
 
 
-const DATA = Array.from({ length: 604 },
-  (v, i) => {
-    let obj = {};
-    obj.id = (i + 1).toString();
-    obj.source = Pages[(i + 1).toString()];
-    return obj;
-  });
-
-console.log('data', DATA)
-
-const Item = ({ source }) => (
-  <Image
-    source={source}
-    style={styles.page}
-  />
-);
 
 const App = () => {
-  const renderItem = ({ item }) => (
-    <Item source={item.source} />
-  );
 
   return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={DATA}
-        renderItem={renderItem}
-        keyExtractor={item => item.id}
-      />
-    </SafeAreaView>
+    <View style={styles.container}>
+      <StatusBar />
+      <Quran />
+    </View>
   );
 }
 
@@ -45,21 +24,13 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'red',
+    backgroundColor: 'rgb(255, 254, 219)',
     marginTop: StatusBar.currentHeight || 0,
   },
   paragraph: {
-    fontSize: 100,
+    fontSize: 16,
     textAlign: 'center',
-  },
-  page: {
-    width: '100%',
-    height: undefined,
-    aspectRatio: 0.737,
-  },
-  node: {
-    width: '100%',
-  },
+  }
 });
 
 export default App;
